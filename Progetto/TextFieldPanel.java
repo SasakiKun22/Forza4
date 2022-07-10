e51f5c138278a522cb02b11d6dfe29fe4d05931a
germano02 <103180372+germano02@users.noreply.github.com>
2022-07-10 17:49:34 +0200

    Update TextFieldPanel.java

--- Progetto/TextFieldPanel.java
+++ Progetto/TextFieldPanel.java
@@ -1,9 +1,27 @@
 import java.awt.*;
 import javax.swing.*;
 
+
+/* The following class, which extends jpanel,
+ * deals with the creation of the two textual boxes for
+ * the insertion of player names.
+ */
+
+
 public class TextFieldPanel extends JPanel {
+    //instantiation of variables
     private JTextField textField1;
     private JTextField textField2;
+
+/**@costructor TextFieldPanel()
+ * The two textAree are defined by assigning a default text.
+ * The size, background color and text font are set using the
+ * setSize(), setBackground(), setfont() methods.
+ * A Layout is defined and is set as FlowLayout().
+ * In addition, the two textField (textField1 and textField2 )
+ * are added to the jpanel via the add method.*/
+
+
 
     public TextFieldPanel() {
 
@@ -19,7 +37,11 @@ public class TextFieldPanel extends JPanel {
     add(textField1);
     add(textField2);
     }
-
+    /**@method getNamePLayer()
+     *  At the end a method named getNamePlayer
+     *  is created to take text from textField
+     * @param playerName the name of the player
+     */
     public String getNamePlayer(String playerName)
     {
         if (playerName == "PLAYER1")
